14662c9fe662a9b8b496153e8907202b484250b9
odgerelight <odgerelariunbold719@gmail.com>
2024-10-30 00:26:31 +0800

    eslint error fix v1

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-// next.config.ts
-import { NextConfig } from "next"
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 }
 
 export default nextConfig
